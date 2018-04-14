f3f9603ff5c5ea137da477d364122bdebee8143a
kingdo777 <1440852110@qq.com>
2018-04-14 14:57:52 +0800

    使用10h中断功能打印字符串以减小boot.bin大小

--- boot/boot.asm
+++ boot/boot.asm
@@ -6,7 +6,7 @@ jmp BOOT_START
 nop;此指令必不可少
 %include "include/fat12_head_info.inc";引入Fat12的头部信息
 loaderName: db 	'loader  bin';中间是空格，因为fat12文件系统仅仅支持8字节文件名和3字节扩展名
-errorInfo:	db	'no Loader',0
+errorInfo:	db	'no Loader'
 
 BOOT_START:
 	mov ax,cs
@@ -47,9 +47,19 @@ loopInSector:;在扇区中循环，扇区512字节，16个目录项
 	pop ax
 	inc ax
 	loop loopInRootDir
-	mov ah,back_black+front_red
-	mov ax,errorInfo
-	call put_string
+	;打印错误信息，为了节省空间，直接调用13号中断
+	;清屏(为了方便直接切换活动页)
+	mov ax,0501h
+    int 0x10
+	;打印字符串
+	xor ax,ax
+	mov es,ax
+	mov bp,errorInfo
+	mov cx,9
+	xor	dx,dx
+	mov bh,01h
+	mov bl,04h
+	mov ah,0x13
 	hlt
 findLoader:;找到了loader.bin所在的项
 	and di,00100000b
@@ -75,12 +85,10 @@ LoadeLoaderEnd:;加载结束
 ;函数：给定逻辑扇区号，从软盘中读取一个扇区并加载到相应内存中
 ;参数：al-->逻辑扇区号  es:bx-->内存地址
 %include	"lib/ReadSector.asm"
-;打印字符串的相关的一系列函数
-;输入：ah->高亮;[ds:ebx]->字符串地址（字符串必须以0结尾）
-;输出：无
-%include	"lib/put_string.asm"
 ;函数：通过fat获取文件的下一簇号
 ;输入：ax-->当前簇号  es:bx要提供一个512字节的空间用来加载FAT
 ;输出：ax-->下一簇号
 %include	"lib/GetNextClusByFat.asm"
+
+times (510-($-$$)) db 0
 BOOT_END:dw 0xaa55
